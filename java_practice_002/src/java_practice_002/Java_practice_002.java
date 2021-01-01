6b1d01d05026e0e29b32dc1ad724dd5e3652d93f
jawad@jawad.com <59015763+jawadaminkp@users.noreply.github.com>
2021-01-01 22:10:32 +0500

    sum operation performed

---
+++ java_practice_002/src/java_practice_002/Java_practice_002.java
@@ -0,0 +1,26 @@
+
+package java_practice_002;
+import java.util.Scanner;
+
+public class Java_practice_002 {
+
+  
+    public static void main(String[] args) {
+      
+        
+        Scanner askme= new Scanner(System.in);
+        
+        System.out.print("Enter 1st number:");
+        int num1 = askme.nextInt();
+        
+        
+        System.out.print("Enter 2nd number:");
+        int num2= askme.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.print("Sum is: "+sum );
+        
+        System.out.println("@javadaminkp");
+    }
+    
+}
